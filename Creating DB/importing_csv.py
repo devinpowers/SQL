fa5f946b1dde1902f9961c9c45abda4ad37b7c27
Devin Powers <50432490+devinpowers@users.noreply.github.com>
2021-01-31 09:05:32 -0500

    Update importing_csv.py
    update

--- Creating DB/importing_csv.py
+++ Creating DB/importing_csv.py
@@ -9,18 +9,22 @@ Created on Wed Jun 24 15:48:37 2020
 
 import sqlite3, csv, pandas
 
+#Connect to the DataBase
 connection  = sqlite3.connect('citi.db')
 cursor = connection.cursor()
 
+#Open the CSV file were working with
 with open('citibike.csv', 'r') as file:
     no_records = 0
+    next(file) ## skip header row
     for row in file:
+        
         cursor.execute("INSERT INTO CitiBikes VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", row.split(","))
         
         connection.commit()
+        
         no_records +=1
         
 connection.close()
-print('\n{} Records Transferred'.format(no_records))
 
-pandas.read_sql_query('SELECT count * FROM CitiBikes ')
+print('\n{} Records Transferred'.format(no_records))
