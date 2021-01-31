730f98a7f111223a90e6ed8c146bd0964a5d04da
Devin Powers <50432490+devinpowers@users.noreply.github.com>
2021-01-31 08:58:44 -0500

    Update create_database.py

--- Creating DB/create_database.py
+++ Creating DB/create_database.py
@@ -5,14 +5,22 @@ Created on Wed Jun 24 12:06:00 2020
 
 @author: devinpowers
 """
+"""
+Creating a Database in SQlite 
+"""
 
 
-import sqlite3, csv
+import sqlite3
+
+# .connect("name_of_data_base_to_Create.db")
 
 conn = sqlite3.connect('citi.db')
+
+## Cursor enables traversal over the records in our database Cursor Objects
 cur = conn.cursor()
 
-
+# We create a table in our DataBase and label each of the headers
+## For each column in our CSV File
 sql = """
     CREATE TABLE CitiBikes (
         Trip_Duration INTEGER,
@@ -26,17 +34,19 @@ sql = """
         EndStationName TEXT,
         EndStationLat DECIMAL,
         EndStationlog DECIMAL,
-        t INTEGAR,
+        BikeId INTEGAR,
         Usertype TEXT,
         Birthyear TEXT,
         Gender INTEGER
         
         )
     """
-    
+#execute our SQL statement
 cur.execute(sql)
 print('Database has been created')
 
+# Commits Current transaction
 conn.commit()
+#Closes the connection to the DataBase
 conn.close()
     
